4078aaa499f938ee7e6f1479ea348bb4b72bbd7b
Elin Nyman <elin.nyman87@gmail.com>
2026-02-19 12:13:26 +0100

    remove page

--- src/pages/private/evenemang.tsx
+++
@@ -1,9 +0,0 @@
-function Evenemang() {
-	return (
-		<div className="flex flex-col justify-center items-center h-full w-full">
-			<h1 className="text-3xl font-stretch-90% text-gray-800">Evenemang</h1>
-		</div>
-	);
-}
-
-export default Evenemang;
